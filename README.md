107d1129bc72f28274a907dca32eed834c586915
Sosarenaint <93826368+Sosarenaint@users.noreply.github.com>
2022-07-22 14:19:15 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Corona Virus Tracker Web Application
+
+### Image
+
+<img src='./images/virus.png' alt="Covid19 WebApp"/>
+
+### Link: https://online-corona-tracker.web.app/
+
+### _COVID-19 Tracker Application that displays, current infected, recovered and death cases due to corona virus, Globally and Country wise with Charts and explainations._
+
+## Task
+
+Built this app to share awareness to people of the world about this Pandemic! , and to make good use of my Web Development skills.
+
+## Functionality
+
+- View Global Data with Line chart
+- View Country specific data with Bar Charts
+- View Current Date of cases
+
+## Run Locally
+
+- Run this command `git clone https://github.com/sosarenaint/Corona-Virus-Tracker.git`
+- You are now in the dev environment and you can play around
+
+## Tech Stack
+
+- HTML5
+- CSS3
+- Javascript ES6
+- React
+- React Hooks
+- Material-UI
+- Chart Js
+- Covid-19 API
+- VS Code
+- Surge
+- Github Actions
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "../../App.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <hr />
+      <hr />
+      <div className="page-wrapper"></div>
+      <footer>
+        <div className="footer">
+          <div className="footer-content">
+            <div className="footer-section about">
+              <h1 className="logo-text">
+                <span className="logo-span">S</span>amson &nbsp;
+                <span className="logo-span">S</span>anyaolu
+              </h1>
+              <p className="footer-para">
+                I am a creative, Web Developer with command on Latest Tech
+                stacks
+              </p>
+              <div className="contact">
+                <span>
+                  <i className="fas fa-envelope"></i>&nbsp; samsonosanyaolu@gmail.com
+                </span>
+              </div>
+              <div className="socials">
+                <a
+                  href="https://www.facebook.com/Praiseloading"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/samson-sanyaolu/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+                <a
+                  href="https://twitter.com/praiseloading"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a
+                  href="https://github.com/sosarenaint"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="footer-bottom">&copy; | Designed By Samson Sanyaolu</div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
